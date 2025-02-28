4ba91ef51627a519019dfa0f1126bb1b5d589404
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-02-28 14:52:25 +0100

    Add: TicketRepository with ITicketRepository interface for data access

---
+++ Tickify/Repositories/ITicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using Tickify.Models;
+
+namespace Tickify.Repositories
+{
+    public interface ITicketRepository
+    {
+        Task<IEnumerable<Ticket>> GetAllTicketsAsync();
+        Task<Ticket> GetTicketByIdAsync(int id);
+        Task AddTicketAsync(Ticket ticket);
+        void UpdateTicket(Ticket ticket);
+        void DeleteTicket(Ticket ticket);
+        Task SaveChangesAsync();
+
+        // Specifikus metódusok, ha szükséges:
+        Task<IEnumerable<Ticket>> GetTicketsByUserAsync(string userId);
+    }
+
+}
