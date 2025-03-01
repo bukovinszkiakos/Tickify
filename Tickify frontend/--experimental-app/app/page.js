4a612bbea863a595b412e9b851d352a2416a63cc
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-03-01 14:52:30 +0100

    feat: add home page with welcome message and app description

---
+++ Tickify frontend/--experimental-app/app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/HomePage.css";  
+
+export default function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Tickify!</h1>
+      <p>This is a simple Issue Tracker Web Application.</p>
+      <p>Use the menu to Login or Register, then manage your tickets.</p>
+    </div>
+  );
+}
