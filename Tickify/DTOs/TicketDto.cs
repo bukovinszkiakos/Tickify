f3b8efe666caf544fb4cd19f78810dddb1072083
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-03-01 14:10:33 +0100

    feat: add ImageUrl to TicketDto

--- Tickify/DTOs/TicketDto.cs
+++ Tickify/DTOs/TicketDto.cs
@@ -11,6 +11,6 @@
         public string Priority { get; set; }
         public string CreatedBy { get; set; }
         public string? AssignedTo { get; set; }
+        public string? ImageUrl { get; set; }
     }
-
 }
